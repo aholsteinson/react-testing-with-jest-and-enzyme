911dc5b93dd9e2364cd93f075f51bd2aef7d97de
Arsen Holsteinson <arsen@phixarmedia.net>
2019-09-15 18:44:47 -0400

    added bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
+class App extends Component {
   render() {
     return (
-      <div data-test="component-app">
-        
-
+      <div className="container" data-test="component-app">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]} />
       </div>
     );
   }
